7c58d1c8528b0aa61b01d1f57cc568990a0c4905
Oskar <oskarrodholm@msn.com>
2020-07-13 09:59:36 +0200

    Remove some tapdances

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/precondition/keymap.c
@@ -644,7 +644,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            REDO, UNDO  , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,    KC_F6 , KC_F7 , KC_F8 , KC_F9 , KC_F10, KC_F11,
          KC_TAB, KC_Q  , KC_W  , KC_F  , KC_P  , KC_B  ,    KC_J  , KC_L  , KC_U  , KC_Y  ,KC_SCLN,KC_MINS,
          KC_ESC, HOME_A, HOME_R, HOME_S, HOME_T, KC_G  ,    KC_M  , HOME_N, HOME_E, HOME_I, HOME_O,KC_QUOT,
-      KC_BSLASH, KC_Z  , KC_X  , KC_C  , HOME_D, KC_V  ,    KC_K  , HOME_H,KC_COMM,TD(TD_DOT),KC_SLSH, KC_GRV,
+      KC_BSLASH, KC_Z  , KC_X  , KC_C  , HOME_D, KC_V  ,    KC_K  , HOME_H,KC_COMM, KC_DOT,KC_SLSH, KC_GRV,
                       KC_BSLASH,ARROW_R,                                   KC_RALT, KC_APP,
                                           NAV  , KC_SPC,    KC_BSPC, SYM_ENT,
                                    TD(CA_CC_CV), MOUSE ,    KC_DEL , KC_APP ,
@@ -655,7 +655,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,    KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_BSPC,
         KC_DOT , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,    KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
-        KC_TILD,TD(TD_EXCLM), KC_AT ,KC_HASH,KC_DLR ,KC_PERC,    KC_CIRC,KC_AMPR,KC_ASTR,KC_EQL ,KC_PLUS,KC_MINS,
+        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,    KC_CIRC,KC_AMPR,KC_ASTR,KC_EQL ,KC_PLUS,KC_MINS,
         _______,_______,_______,_______,_______,_______,    _______,_______,_______,_______,_______,_______,
                         _______,_______,                                    _______,_______, 
                                         _______,KC_UNDS,    _______,_______,

59fee35b9c348b6153a571904d1e951ed3b1a3c3
Ibnu D. Aji <iaji@siskam.link>
2021-06-25 14:08:03 +0700

    slepet kibot

--- keyboards/slepet/keymaps/default/config.h
+++ keyboards/slepet/keymaps/default/config.h
@@ -17,26 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID 0x1209
-#define PRODUCT_ID 0x6969
-#define DEVICE_VER 0x0001
-#define MANUFACTURER Rick
-#define PRODUCT Something
-
-/* key matrix size */
-#define MATRIX_ROWS 4
-#define MATRIX_COLS 10
-
-#define TAPPING_TERM    200
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
-
-#define DEBOUNCE    5
-
-#define USB_MAX_POWER_CONSUMPTION 500
-
-/* NKRO */
-#ifndef FORCE_NKRO
-    #define FORCE_NKRO  // Depends on NKRO_ENABLE.
-#endif
-
+#define COMBO_TERM  50
+#define COMBO_COUNT 38
+#define IGNORE_MOD_TAP_INTERRUPT
+#define PERMISSIVE_HOLD
---
+++ keyboards/slepet/slepet.h
@@ -0,0 +1,45 @@
+/* Copyright 2021 Ibnu D. Aji
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "quantum.h"
+
+/* This is a shortcut to help you visually see your layout.
+ *
+ * The first section contains all of the arguments representing the physical
+ * layout of the board and position of the keys.
+ *
+ * The second converts the arguments into a two-dimensional array which
+ * represents the switch matrix.
+ */
+#define XXX KC_NO
+
+#define LAYOUT( \
+    k00, k01, k02, k03, k04, k05,      k45, k44, k43, k42, k41, k40, \
+    k10, k11, k12, k13, k14, k15,      k55, k54, k53, k52, k51, k50, \
+    k20, k21, k22, k23, k24, k25,      k65, k64, k63, k62, k61, k60, \
+              k31, k32, k33, k34, k35, k75, k74, k73, k72, k71       \
+) { \
+    { k00, k01, k02, k02, k04, k05 }, \
+    { k10, k11, k12, k13, k14, k15 }, \
+    { k20, k21, k22, k23, k24, k25 }, \
+    { XXX, k31, k32, k33, k34, k35 }, \
+    { k40, k41, k42, k43, k44, k45 }, \
+    { k50, k51, k52, k53, k54, k55 }, \
+    { k60, k61, k62, k63, k64, k65 }, \
+    { XXX, k71, k72, k73, k74, k75 }, \
+}

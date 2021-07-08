ec5353d7391ac168f95cba18489410d833f925c6
Ibnu D. Aji <iaji@siskam.link>
2021-07-08 21:38:22 +0700

    led for 5x6

---
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/led/config.h
@@ -0,0 +1,33 @@
+/*
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+//#define EE_HANDS
+// Rows are doubled-up
+
+#undef RGBLED_NUM
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 10
+#define RGBLED_SPLIT {5, 5}
+#define RGBLIGHT_ANIMATIONS
